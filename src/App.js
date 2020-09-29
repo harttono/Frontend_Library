b53e4eb0603c1abc4c681a4a9e1ac112922808f1
harttono <hartono240896@gmail.com>
2020-09-30 02:08:04 +0700

    dashboard page done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
     <Switch>
-      <Route path='/' exact component={Home}/>
+      <Route path='/' exact component={Dasboard}/>
     </Switch>
   </Router>
   );
