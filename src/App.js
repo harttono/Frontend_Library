4acc44f719bf1bbbd610e090b91c19b83612f3cc
harttono <hartono240896@gmail.com>
2020-09-30 00:06:10 +0700

    add register page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch,BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './Components/Home';
 import Dasboard from './Components/Dashboard';
-import Dashboard from './Components/Dashboard';
+
 
 function App() {
   return (
